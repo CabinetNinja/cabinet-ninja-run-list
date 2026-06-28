53842018a8f92b5240e40847ca0384892f2707d2
CabinetNinja <info@cabinetninja.co.nz>
2026-06-28 19:43:40 +1200

    Update run-list-config.js

--- run-list-config.js
+++ run-list-config.js
@@ -1,6 +1,6 @@
 window.RUN_LIST_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://xoyzmjbjbaknvgtoofar.supabase.co",
+  supabaseAnonKey: "sb_publishable_sRTlYbg7ZIH8iz6QnoNRUA_Iwm1Nf4D",
   requireAuth: true,
   seedRemoteOnFirstRun: true,
 };
