df931f62b121c5a0611bf36a575c3da751ad7dba
adamants-electrical <85815279+adamants-electrical@users.noreply.github.com>
2026-07-11 21:41:04 +1200

    Bust Run List app cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cabinet-ninja-run-list-v7";
+const CACHE_NAME = "cabinet-ninja-run-list-v8";
 const ASSETS = [
   "./",
   "./index.html",
@@ -25,6 +25,20 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  const appShellAsset = ASSETS.some((asset) => url.pathname.endsWith(asset.replace("./", "/cabinet-ninja-run-list/")));
+  if (appShellAsset) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+    );
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then((cached) =>
       cached || fetch(event.request).catch(() => caches.match("./index.html"))
